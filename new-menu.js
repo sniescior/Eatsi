a5ba9eb4fc745bc272ccd9d13cad0ead97889153
PseudonymSN <sniescior160@gmail.com>
2021-11-17 21:47:30 +0100

    new menu updates

--- new-menu.js
+++ new-menu.js
@@ -1,6 +1,7 @@
-let menu = document.getElementById('menu');
-let button = document.getElementById('menu-open-button');
-let navbar = document.getElementById('nav');
+const menu = document.getElementById('menu');
+const button = document.getElementById('menu-toggle-button');
+const buttonIcon = document.getElementById('menu-toggle-button-icon')
+const navbar = document.getElementById('nav');
 
 let toggleMenu = () => {
     navbar.classList.toggle('menu-expanded');
@@ -8,3 +9,19 @@ let toggleMenu = () => {
     button.classList.toggle('icon-menu');
     button.classList.toggle('icon-cancel');
 }
+
+// hide menu on scroll
+window.addEventListener('scroll', (e) => {
+    if(menu.classList.contains('visible')) toggleMenu();
+});
+
+// hide menu on click anywhere outside the menu or navbar element
+window.addEventListener('click', (e) => {
+    if(e.target.classList.contains('menu-wrapper')) return;
+    if(e.target == button) return;
+    if(e.target == navbar) return;
+    else {
+        if(menu.classList.contains('visible')) toggleMenu();
+        return;
+    }
+});
