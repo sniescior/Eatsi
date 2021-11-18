2b8812e53e939a7e8003e2d83bc5834c9311a084
PseudonymSN <sniescior160@gmail.com>
2021-11-18 10:12:42 +0100

    new menu RWD improvements

--- new-menu.js
+++ new-menu.js
@@ -1,13 +1,20 @@
 const menu = document.getElementById('menu');
-const button = document.getElementById('menu-toggle-button');
+const openButton = document.getElementById('menu-open-button');
+const closeButton = document.getElementById('menu-close-button');
 const buttonIcon = document.getElementById('menu-toggle-button-icon')
 const navbar = document.getElementById('nav');
+const container = document.getElementById('container');
+const auxiliaryNav = document.getElementById('auxiliary-nav');
 
 let toggleMenu = () => {
     navbar.classList.toggle('menu-expanded');
     menu.classList.toggle('visible');
-    button.classList.toggle('icon-menu');
-    button.classList.toggle('icon-cancel');
+    openButton.classList.toggle('icon-menu');
+    openButton.classList.toggle('icon-cancel');
+    closeButton.classList.toggle('visible');
+    container.classList.toggle('menu-expanded');
+    auxiliaryNav.classList.toggle('menu-expanded');
+    document.body.classList.toggle('block-scroll');
 }
 
 // hide menu on scroll
@@ -18,7 +25,8 @@ window.addEventListener('scroll', (e) => {
 // hide menu on click anywhere outside the menu or navbar element
 window.addEventListener('click', (e) => {
     if(e.target.classList.contains('menu-wrapper')) return;
-    if(e.target == button) return;
+    if(e.target == openButton) return;
+    if(e.target == closeButton) return;
     if(e.target == navbar) return;
     else {
         if(menu.classList.contains('visible')) toggleMenu();
